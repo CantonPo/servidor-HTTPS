9095919970a3f082292712892f446f0c62aa7fda
CantonPo <cantoncorpas@gmail.com>
2025-02-06 11:41:50 +0100

    cambios 2

--- server.js
+++ server.js
@@ -1,14 +1,42 @@
 const express = require('express');
-
 const app = express();
+app.use(express.json());
+const {getMessages, addMessage} = require('./database.js');
 
-function holaMundo(request, response){
-    console.log("Hola mundo") //esto se ejecuta en el servidor
-    response.send("Hola mundo") //esto se devuelve al cliente
-}
+const APIKEY = "123456";
 
-app.get("/", holaMundo)
+app.get('/', (req, res) => {
+  res.send('Bienvenido al despliegue del servidor de Alvaro!');
+})
 
-app.listen(3000, () => {
-    console.log("El servidor está funcionando en el puerto 3000")
+app.get('/message', (req, res) => {    
+   // Devolver mensajes alamacenados en la BBDD
+   const apikey = req.headers['apikey'];
+   if (apikey !== APIKEY){
+     return res.status(401).send('Unauthorized');
+    }else if (apikey === APIKEY){
+      res.json(getMessages());
+      return res.status(200).send('OK');  
+   }
+})
+
+app.post('/message', (req, res) => {    
+   // Guardar mensajes en la BBDD
+   const apikey = req.headers['apikey'];
+  if (apikey !== APIKEY) {
+    return res.status(401).send('Unauthorized');
+  }
+
+  // Manda el mensaje por la query y lo añadade a la BBDD
+  const message = req.body.message;  
+  if (message) {
+    addMessage(message);
+    res.status(201).send('Message added');
+  } else {
+    res.status(400).send('Bad Request');
+  }
+})
+
+app.listen(3000, () => {    
+    console.log('Servidor corriendo en http://localhost:3000');
 })
---
+++ test.sh
@@ -0,0 +1,2 @@
+curl -X POST -H "Content-Type: application/json" -H "apikey: 123456" -d '{"message": "Un mensaje"}' http://dev1.cyberbunny.online:3000/message
+curl -H "apikey: 123456" http://dev1.cyberbunny.online:3000/message
